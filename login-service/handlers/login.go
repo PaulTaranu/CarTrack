f5924e96e9f61514b1d34050d703845fc6cd12fe
tarpaul <paul31356@gmail.com>
2025-05-03 17:34:50 +0300

    feat: implemented login handler

---
+++ login-service/handlers/login.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/PaulTaranu/CarTrack/login-service/auth"
+	"github.com/PaulTaranu/CarTrack/login-service/models"
+	"github.com/labstack/echo/v4"
+)
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func LoginHandler(c echo.Context) error {
+	var req LoginRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid Request"})
+	}
+
+	user, err := models.FindUsersByEmail(req.Email)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Internal error"})
+	}
+
+	if user == nil || !auth.CheckPassword(req.Password, user.Password) {
+		return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Invalid credentials"})
+	}
+
+	token, err := auth.GenerateToken(user.ID, user.Email)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "JWT generation failed"})
+	}
+	return c.JSON(http.StatusOK, LoginResponse{Token: token})
+}
